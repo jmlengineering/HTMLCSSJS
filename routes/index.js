615b0d92c000a5b742c9393b5b89bd49e2e05562
Jasmine M. Lee <jasmineleeengineering@gmail.com>
2022-09-24 20:44:51 -0400

    BOOKMVC-routes
    route files (auth and index)

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+// @desc Loging and Landing page
+// @route Get /
+router.get('/', (req, res) => {
+    res.render('login', {
+        layout: 'login', 
+    })
+})
+
+// @desc Dashboard
+// @route Get /dashboard
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
+
+module.exports  = router
